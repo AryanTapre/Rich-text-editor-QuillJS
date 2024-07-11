1a3ad9a7f1e2330e28971d2c19edae12f1ec3508
AryanTapre <taprearyan7@gmail.com>
2024-07-11 19:02:49 +0530

    toolbar options added for snow.css theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import Sources from "quill"
 import {Editor} from './components'
 import {UnprivilegedEditor} from "react-quill";
 
-
 const App = () => {
-    
 
     const handler = (content:string,delta:DeltaStatic,source:Sources,editor:UnprivilegedEditor) => {
         console.log("text-change: "+editor.getText());
@@ -20,6 +18,7 @@ const App = () => {
             <Editor 
                 handler={handler}  
                 selectionHandler={selectionHandler} 
+                value="welcome"
             />
         </>
     )
--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -2,24 +2,32 @@ import ReactQuill from 'react-quill';
 import "react-quill/dist/quill.snow.css";
 
 //@ts-expect-error onchangeHandler
-function Editor({handler,selectionHandler}) {
+function Editor({handler,selectionHandler,value}) {
   return (
     <div>
-          <div className="  justify-center flex  h-screen w-screen bg-gray-100  ">
+          <div className="justify-center flex  h-screen w-screen bg-gray-100  ">
             <div className="w-full mx-52 mt-6  shadow-lg">
                 <ReactQuill
                     theme="snow"
-                    modules={{toolbar:true}}
-                    value="hello"
+                    modules={{toolbar:[
+                      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                      [{ 'font': [] }],
+                      ['bold', 'italic', 'underline'],
+                      [{ 'list': 'ordered'}, { 'list': 'bullet' },{ 'list': 'check' }],
+                      [{ 'script': 'sub'}, { 'script': 'super' }], 
+                      [{ 'indent': '-1'}, { 'indent': '+1' }],
+                      ['link', 'image','video', 'formula'],
+                      [{'color':[]},{'background':[]}],
+                      ['blockquote', 'code-block'],
+                      [{ 'header': 1 }, { 'header': 2 }],   
+                      [{ 'align': [] }],
+                      
+                    ]}}
+                    value={value}
                     onChange={handler}
                     onChangeSelection={selectionHandler}
                     style={{height:'90%'}}
-                    formats={ [
-                      'header',
-                      'bold', 'italic', 'underline', 'strike', 'blockquote',
-                      'list', 'bullet', 'indent',
-                      'link', 'image'
-                    ]}
+                    
                 />
             </div>
           </div>
